546438d0320ed9776821532e2ab586f98e7e368a
Alef Sloan <Alef_sloan@hotmail.com>
2022-06-30 20:29:03 -0300

    feat: final exercise 3

---
+++ exercises/Bloco_33/Dia_2/conteudo_final/exercise-3.py
@@ -0,0 +1,34 @@
+import random
+
+str = 'source-for-exercise/normal-exercise-3-final/random_words.txt'
+file = open(str, mode='r')
+content = file.readlines()
+arr_with_words = []
+
+for x in content:
+    arr_with_words.append(x.strip())
+
+file.close()
+
+
+def scramble_game():
+    random_word = random.choice(arr_with_words)
+    play_word = "".join(random.sample(random_word, len(random_word)))
+    number_of_tries = 0
+
+    print(play_word)
+
+    guess_word = input('Digite a palavra na ordem correta: ')
+
+    while guess_word != random_word and number_of_tries < 2:
+        guess_word = input('Digite a palavra na ordem correta: ')
+        number_of_tries += 1
+
+    if guess_word != random_word:
+        print(f"Que pena, a palavra correta era { random_word }, você perdeu!")
+    else:
+        print(f"Parabéns, você acertou a palavra correta { random_word }")
+
+
+if __name__ == '__main__':
+    scramble_game()
